ee4cb45d02a3d120094828dfeab324dc62c67b06
madhurikapse <120490305+madhurikapse@users.noreply.github.com>
2025-01-09 13:35:07 +0530

    update

--- frontend/src/components/Navbar3.js
+++ frontend/src/components/Navbar3.js
@@ -10,11 +10,10 @@ const Navbar3 = () => {
                     <li className="nav-item">FICTION</li>
                     <li className="nav-item">NON-FICTION</li>
                     <li className="nav-item">TEEN AND YOUNG ADULT</li>
-                    <li className="nav-item">#BOOKTOK</li>
+                    <li className="nav-item">BOOKTOK</li>
                     <li className="nav-item">PAN AFRICAN</li>
                     <li className="nav-item">GIFT CARTS AND STATIONERY</li>
                     <li className="nav-item">CRIME FICTION</li>
-                    <li className="nav-item">MIND BODY SPIRIT</li>
                 </ul>
             </div>
         </div>
